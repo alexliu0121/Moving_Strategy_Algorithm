6df70160b0f45ded930b4a3c62b14da30d935a1e
AlexLCT <45057088+alexliu0121@users.noreply.github.com>
2025-03-25 15:37:43 -0700

    Delete A1073311_checkpoint7_Fringe.java

--- A1073311_checkpoint7_Fringe.java
+++
@@ -1,8 +0,0 @@
-public interface A1073311_checkpoint7_Fringe{
-    //Description : add the input object into Fringe
-    void add(A1073311_checkpoint7_Block block);
-    //Description : return and remove the object from Fringe.
-    A1073311_checkpoint7_Block remove();
-    //Description : Check if the Fringe has a object at least. if it is empty return true, vice versa. 
-    boolean isEmpty();
-}
